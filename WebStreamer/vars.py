679962f7b7c7910ce2f67b8c223195d81e66383d
noahsarkq <86694875+noahsarkq@users.noreply.github.com>
2022-02-04 08:36:02 +0530

    Add files via upload

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -13,10 +13,11 @@ class Var(object):
     API_HASH = str(environ.get("API_HASH"))
     BOT_TOKEN = str(environ.get("BOT_TOKEN"))
     SLEEP_THRESHOLD = int(environ.get("SLEEP_THRESHOLD", "60"))  # 1 minte
-    WORKERS = int(environ.get("WORKERS", "6"))  # 6 workers = 6 commands at once
-    BIN_CHANNEL = int(
-        environ.get("BIN_CHANNEL", None)
-    )  # you NEED to use a CHANNEL when you're using MULTI_CLIENT
+    WORKERS = int(environ.get("WORKERS",
+                              "6"))  # 6 workers = 6 commands at once
+    BIN_CHANNEL = int(environ.get(
+        "BIN_CHANNEL",
+        None))  # you NEED to use a CHANNEL when you're using MULTI_CLIENT
     PORT = int(environ.get("PORT", 8080))
     BIND_ADDRESS = str(environ.get("WEB_SERVER_BIND_ADDRESS", "0.0.0.0"))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
@@ -24,19 +25,19 @@ class Var(object):
     HAS_SSL = True if str(HAS_SSL).lower() == "true" else False
     NO_PORT = environ.get("NO_PORT", False)
     NO_PORT = True if str(NO_PORT).lower() == "true" else False
+    AUTOHORISED = str(environ.get("AUTHORISED"))
+    DETA = [int(xd) for xd in AUTOHORISED.split(",")]
+    HASH0 = str(environ.get("HASH"))
+    HASH = [yd for yd in HASH0.split(",")]
     if "DYNO" in environ:
         ON_HEROKU = True
         APP_NAME = str(environ.get("APP_NAME"))
     else:
         ON_HEROKU = False
-    FQDN = (
-        str(environ.get("FQDN", BIND_ADDRESS))
-        if not ON_HEROKU or environ.get("FQDN")
-        else APP_NAME + ".herokuapp.com"
-    )
+    FQDN = (str(environ.get("FQDN", BIND_ADDRESS)) if not ON_HEROKU
+            or environ.get("FQDN") else APP_NAME + ".herokuapp.com")
     if ON_HEROKU:
         URL = f"https://{FQDN}/"
     else:
-        URL = "http{}://{}{}/".format(
-            "s" if HAS_SSL else "", FQDN, "" if NO_PORT else ":" + str(PORT)
-        )
+        URL = "http{}://{}{}/".format("s" if HAS_SSL else "", FQDN,
+                                      "" if NO_PORT else ":" + str(PORT))
