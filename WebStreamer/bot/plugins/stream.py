3a98f67e0d527b3d6fa4fe2d502f29a8c20d0783
noahsarkq <86694875+noahsarkq@users.noreply.github.com>
2022-02-04 08:35:47 +0530

    Add files via upload

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -8,6 +8,7 @@ from urllib.parse import quote_plus
 from WebStreamer.bot import StreamBot
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from WebStreamer.utils.file_id import get_unique_id
+import random
 
 
 def detect_type(m: Message):
@@ -22,15 +23,13 @@ def detect_type(m: Message):
 
 
 @StreamBot.on_message(
-    filters.private
-    & (
-        filters.document
-        | filters.video
-        | filters.audio
-        | filters.animation
-        | filters.voice
-        | filters.video_note
-    ),
+    filters.private & filters.user(Var.DETA)
+    & (filters.document
+       | filters.video
+       | filters.audio
+       | filters.animation
+       | filters.voice
+       | filters.video_note),
     group=4,
 )
 async def media_receive_handler(_, m: Message):
@@ -38,17 +37,14 @@ async def media_receive_handler(_, m: Message):
     file_name = ""
     if file:
         file_name = file.file_name
-    log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
-    stream_link = f"{Var.URL}{log_msg.message_id}/{quote_plus(file_name)}?hash={get_unique_id(log_msg)}"
-    short_link = f"{Var.URL}{get_unique_id(log_msg)}{log_msg.message_id}"
+    log_msg = await m.copy(chat_id=Var.BIN_CHANNEL)
+    stream_link = f"{Var.URL}{log_msg.message_id}/{random.choice(Var.HASH)}"
+    # short_link = f"{Var.URL}{get_unique_id(log_msg)}{log_msg.message_id}"
     logging.info(f"Generated link: {stream_link} for {m.from_user.first_name}")
     await m.reply_text(
-        text="<code>{}</code> (<a href='{}'>shortened</a>)".format(
-            stream_link, short_link
-        ),
+        text="<code>{}</code>".format(stream_link),
         quote=True,
         parse_mode="html",
         reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("Open", url=stream_link)]]
-        ),
+            [[InlineKeyboardButton("Open", url=stream_link)]]),
     )
